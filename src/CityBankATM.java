d46c76506994115be74b3bf1fc546245cf21760d
Bexultan Kuralbay <30636@iitu.edu.kz>
2023-02-28 20:06:47 +0600

    first

--- src/CityBankATM.java
+++ src/CityBankATM.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class CityBankATM extends CityBankAccount{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("ENTER YOUR ACCOUNT NUMBER");
+        System.out.println("ENTER YOUR ACCOUNT");
         String accountNumberFromConsole = in.next();
         System.out.println("ENTER YOUR PIN CODE");
         String accountPinCodeFromConsole = in.next();
